9a18018d46e138c73d3ffd704dfc0f4969d1b187
Michael Jurke <m.jurke@gmx.de>
2022-04-21 20:39:52 +0200

    Add agent partner button and action

--- models/partner.py
+++ models/partner.py
@@ -31,8 +31,28 @@ class Partner(models.Model):
         inverse_name="agent_id",
     )
 
+    def _compute_agent_partner_count(self):
+        for record in self:
+            record.agent_partner_count = len(record.agent_partner_ids)
+
+    agent_partner_count = fields.Integer(
+        string="Agent Partner Count",
+        compute=_compute_agent_partner_count,
+    )
+
     @api.constrains("agent_id")
     def _reset_agent_id(self):
         if self.is_agent and self.agent_id:
             # silently reset the agent_id for agents
             self.agent_id = None
+
+    def action_open_agent_partners(self):
+        self.ensure_one()
+        action = self.env["ir.actions.actions"]._for_xml_id(
+            "base.action_partner_customer_form"
+        )
+        action["context"] = {
+            "search_default_agent_id": [self.id],
+            "default_agent_id": self.id,
+        }
+        return action
